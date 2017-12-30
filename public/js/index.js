eaa4b03a41dc3f6ec60f86b7b393391c88cfc9d9
Charles Fried <designcharlesfried@gmail.com>
2017-12-30 17:55:18 +0000

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+socket.on('connect', function() {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: "Talk@charlesfried.com",
+    text: 'Hey, this is Charles'
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('New email is available.');
+  console.log(message);
+});
