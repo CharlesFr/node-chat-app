1b526d185324bba600906f8bc6a464087a1fa645
Charles Fried <designcharlesfried@gmail.com>
2017-12-30 15:59:11 +0000

    First commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+var app = express();
+
+
+const publicPath = path.join(__dirname, './../public')
+
+
+app.use(express.static(publicPath));
+
+
+app.listen(3000, () => console.log('App running on port 3000'));
